a0931583f8a4f2a60d3dc0bcba787c87a5078f1a
bashar334 <ba7992@rit.edu>
2022-09-12 18:09:54 +0400

    answer

--- ex.py
+++ ex.py
@@ -1,4 +1,21 @@
-print("hello world")
-print("goodbye")
-print("answer")
-print("okey")
+def days_of_week():
+    print("Sunday","Monday","Tuesday")
+    print("wednesday,thursday,frieday")
+    print("saturday")
+ 
+
+def birthdate():
+    print("july 31st, 1980")
+
+
+
+
+    def box():
+        print("*****")
+        print("*   *")
+        print("*****")
+
+        box()
+        days_of_week()
+        birthdate()
+        
